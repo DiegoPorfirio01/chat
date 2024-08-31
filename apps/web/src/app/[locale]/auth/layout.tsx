d84b73e88d0f65a23db3f74e70558c0826dfbb12
Diego <diego.vianna@rua.com.br>
2024-08-30 23:01:13 -0300

    feat(auth-layout) button back

--- apps/web/src/app/[locale]/auth/layout.tsx
+++ apps/web/src/app/[locale]/auth/layout.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation'
 import { useLocale } from 'next-intl'
 
 import { isAuthenticated } from '@/auth/auth'
+import Link from 'next/link'
+import { ArrowLeftSquare } from 'lucide-react'
 
 export default function AuthLayout({
   children,
@@ -16,6 +18,12 @@ export default function AuthLayout({
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-4">
+      {
+        <Link href={`/${locale}`} className="flex items-center gap-1">
+          <ArrowLeftSquare className="size-6" />
+          <span>Back</span>
+        </Link>
+      }
       <div className="w-full max-w-xs">{children}</div>
     </div>
   )
